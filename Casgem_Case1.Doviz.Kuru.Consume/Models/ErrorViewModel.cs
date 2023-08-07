7c8b9a5d6e3612b383aba89602d63e04a7053455
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-07 20:48:21 +0300

    Add project files.

---
+++ Casgem_Case1.Doviz.Kuru.Consume/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Casgem_Case1.Doviz.Kuru.Consume.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
