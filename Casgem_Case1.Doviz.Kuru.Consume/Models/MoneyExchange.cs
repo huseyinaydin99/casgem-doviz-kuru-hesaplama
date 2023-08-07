d324a057212641f332f2c60afa2660d1d250d563
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-07 20:59:08 +0300

    Consume Layer kısmında MoneyExchance sınıfı olışturuldu.

---
+++ Casgem_Case1.Doviz.Kuru.Consume/Models/MoneyExchange.cs
@@ -0,0 +1,17 @@
+﻿namespace Casgem_Case1.Doviz.Kuru.Consume.Models
+{
+    public class MoneyExchange
+    {
+        public DateTime date { get; set; }
+
+        public decimal? usd { get; set; }
+
+        public decimal? eur { get; set; }
+
+        public decimal? chf { get; set; }
+
+        public decimal? gbp { get; set; }
+
+        public decimal? jpy { get; set; }
+    }
+}
